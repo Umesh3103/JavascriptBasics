c9d524727902520a99a8b13c247470a443d1723a
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-11 15:37:36 +0530

    [Umesh]Added: Add two dice number

--- Sequence/SequenceProblem.js
+++ Sequence/SequenceProblem.js
@@ -5,3 +5,8 @@ console.log(digit);
 // UC2
 let diceNumber = Math.floor((Math.random()*10)%6)+1;
 console.log("Dice Number is: "+diceNumber);
+
+//UC3
+let diceNumber1 = Math.floor((Math.random()*10)%6)+1;
+let diceNumber2 = Math.floor((Math.random()*10)%6)+1;
+console.log("Addition of two dice Number is: "+(diceNumber1+diceNumber2));
