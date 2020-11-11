dcfbb2f573b4b3c47a3853bcfeece57dfa463f0d
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-11 16:01:45 +0530

    [Umesh]Added: Find sum and average of 5 random 2 digit number

--- Sequence/SequenceProblem.js
+++ Sequence/SequenceProblem.js
@@ -10,3 +10,18 @@ console.log("Dice Number is: "+diceNumber);
 let diceNumber1 = Math.floor((Math.random()*10)%6)+1;
 let diceNumber2 = Math.floor((Math.random()*10)%6)+1;
 console.log("Addition of two dice Number is: "+(diceNumber1+diceNumber2));
+
+//UC4
+let digit1 = Math.floor(Math.random()*90)+10;
+console.log(digit1);
+let digit2 = Math.floor(Math.random()*90)+10;
+console.log(digit2);
+let digit3 = Math.floor(Math.random()*90)+10;
+console.log(digit3);
+let digit4 = Math.floor(Math.random()*90)+10;
+console.log(digit4);
+let digit5 = Math.floor(Math.random()*90)+10;
+console.log(digit5);
+let sum = digit1+digit2+digit3+digit4+digit5;
+let avg = sum/5;
+console.log("Sum is: "+sum+", Average is: "+avg);
