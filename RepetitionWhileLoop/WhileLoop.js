f0618aa6a5c2b8e5249e8c852602e17d895e6df3
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-12 18:17:48 +0530

    [Umesh]Added: finding number of bets played in a gambling game for either winnin or loosing

--- RepetitionWhileLoop/WhileLoop.js
+++ RepetitionWhileLoop/WhileLoop.js
@@ -8,17 +8,6 @@ while(i<=n && number<256){
     i++;
 }
 
-//UC2
-/*let magicNumber=25;
-let input = process.argv[3];
-while(input!=magicNumber){
-    if(input<magicNumber)
-        input *= 2;
-    else
-        input /= 2; 
-}
-console.log("Magic Number is reached: "+input);*/
-
 //UC3
 let counter1=0;
 let counter2=0;
@@ -33,4 +22,23 @@ while(counter1!=11 && counter2!=11){
 if(counter1==11)
     console.log("Player who picked heads won");
 else
-    console.log("Player who picked Tails won");    
+    console.log("Player who picked Tails won");  
+    
+
+//UC4
+let money = 100;
+let counter=0;
+while(money>0 && money<200){
+    let m = Math.floor(Math.random()*10)%2;
+    if(m==0)
+        money--;
+    else
+        money++;
+    counter++;        
+}
+console.log(counter+" bets were played");
+if(money==0){
+    console.log("You lost all the money");
+}
+else
+    console.log("You reached your goal "+money);
