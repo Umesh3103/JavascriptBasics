c984970832f4b1dbd0348583c13c9d3e20a0d1dc
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-12 16:50:05 +0530

    [Umesh]Added: Finding if a number is prime or not

--- RepetitionForLoop/ForLoop.js
+++ RepetitionForLoop/ForLoop.js
@@ -12,3 +12,13 @@ for(let i=2;i<=digit;i++){
     harmonicNumber += 1/i;
 }
 console.log(harmonicNumber);
+
+// UC3 
+let p = process.argv[4];
+for(let i = 2; i<= Math.sqrt(p);i++){
+    if(p%i==0){
+        console.log(p+" is not a prime number");
+        return;
+    }    
+}
+console.log(p+" is a prime number");
