855e9dbe73fb17f680de0c2c10b64964fd744da2
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-12 16:36:13 +0530

    [Umesh]Added: Printing harmonic number

--- RepetitionForLoop/ForLoop.js
+++ RepetitionForLoop/ForLoop.js
@@ -4,3 +4,11 @@ for(let i = 1;i<=n;i++){
     let number = Math.pow(2,i);
     console.log(number);
 }
+
+//UC2
+let digit = process.argv[3];
+let harmonicNumber = 1;
+for(let i=2;i<=digit;i++){
+    harmonicNumber += 1/i;
+}
+console.log(harmonicNumber);
