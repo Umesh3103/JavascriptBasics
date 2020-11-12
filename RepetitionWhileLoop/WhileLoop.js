92d14312fc938c65d46a0e1eb8e76d85fdfc7285
DESKTOP-1260D35\Hp <deoraumesh32@gmail.com>
2020-11-12 17:53:45 +0530

    [Umesh]Added: Flipping coin till either heads or tails count comes 11

--- RepetitionWhileLoop/WhileLoop.js
+++ RepetitionWhileLoop/WhileLoop.js
@@ -7,3 +7,30 @@ while(i<=n && number<256){
     console.log(number);
     i++;
 }
+
+//UC2
+/*let magicNumber=25;
+let input = process.argv[3];
+while(input!=magicNumber){
+    if(input<magicNumber)
+        input *= 2;
+    else
+        input /= 2; 
+}
+console.log("Magic Number is reached: "+input);*/
+
+//UC3
+let counter1=0;
+let counter2=0;
+while(counter1!=11 && counter2!=11){
+    let m = Math.floor(Math.random()*10)%2;
+    if(m==0){
+        counter1++;
+    }
+    else
+        counter2++;
+}
+if(counter1==11)
+    console.log("Player who picked heads won");
+else
+    console.log("Player who picked Tails won");    
